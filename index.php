380ff0ca71ad125d0230a73956977fe335e2fedd
Stanskis <siris.gamo@gmail.com>
2024-06-27 23:12:45 +0200

    Jeudi
    committed pour Jeudi

--- index.php
+++ index.php
@@ -8,10 +8,154 @@ $sec = "1";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'"
+    <!-- <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'" -->
 </head>
 <body>
+    <?php
+        include 'src/components/lp_carousel/carte-descript.php';
+    ?>
+    <!-- header -->
+    <span><a class="btn btn-danger" href="src\components\lp_carousel\carte-descript.php">Carte</a></span>
+    <!-- <span><a class="btn btn-danger" href="src\components\lp_carousel\php-config.php">Php-config</a></span> -->
+    <div class="wrapper bg-dark">
+        <section class="container legendary fs-1 bg-success">
+            <h4>Films pour Tous les Temps !</h4>
+            <div id="carouselExampleControls1" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls1" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls1" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>  
+        </section>    
+        <section class="container bg-warning pb-5">
+                <h3>Découvrez les films et les séries les plus populaires, les nouveatés et les prochains sorties</h3>
+                <div class="container row">
+                    <div class="content-text p-4 col">
+                        <h5>Top 10 des films de la semaine</h5>
+                        <p>Découvrez les films les plus populaires de la semaine</p>
+                    </div>
+                    <div class="horizontal-scrollbar col-10 overflow-auto">
+                        <?php
+                            createTop10Film($filmTop);
+                        ?>
+                    </div>
+                </div>
+                <div class="container row">
+                    <div class="content-text p-4 col">
+                        <h5>Nouveaux films</h5>
+                        <p>Découvrez les derniers films sorties</p>
+                    </div>
+                    <div class="horizontal-scrollbar col-10 overflow-auto">
+                        <?php
+                            createNouvFilm($filmNouv);
+                        ?>
+                    </div>
+                </div>
+                <div class="container bg-danger rounder mt-5 p-5">
+                <div id="carouselExampleIndicators" class="carousel info-plus slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="9" aria-label="Slide 10"></button>
+                    </div>
+                    <div class="carousel-inner d-flex">
+                        <?php
+                            createFilmPlus($filmPlus);
+                        ?>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                    </div>
+                </div>
+                <div class="container row">
+                    <div class="content-text p-4 col">
+                        <h5>Top 10 des séries de la semaine</h5>
+                        <p>Découvrez les séries les plus populaires de la semaine</p>
+                    </div>
+                    <div class="horizontal-scrollbar col-10 overflow-auto">
+                        <?php
+                            createTop10Serie($serieTop);
+                        ?>
+                    </div>
+                </div>
+                <div class="container row">
+                    <div class="content-text p-4 col">
+                        <h5>Nouvelles séries</h5>
+                        <p>Découvrez les dernières séries sorties</p>
+                    </div>
+                    <div class="horizontal-scrollbar col-10 overflow-auto">
+                        <?php
+                            createNouvSerie($serieNouv);
+                        ?>
+                    </div>
+                </div>
+                <div class="container bg-danger rounder mt-5 p-5">
+                <div id="carouselExampleIndicators2" class="carousel info-plus slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="8" aria-label="Slide 9"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="9" aria-label="Slide 10"></button>
+                    </div>
+                    <div class="carousel-inner d-flex">
+                        <?php
+                            createSeriePlus($seriePlus);
+                        ?>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                    </div>
+                </div>
+        </section>
+        <!-- footer -->
+    </div>
+    
+
     <?php
        
     ?>
---
+++ src/components/lp_carousel/carte-descript.php
@@ -0,0 +1,109 @@
+<?php
+  include 'php-config.php';
+
+  $sql1 = "SELECT Titre FROM `film` ORDER BY `film`.`Note` DESC LIMIT 10";
+  $requete1 = $db->query($sql1);
+  $filmTop = $requete1->fetchAll();
+
+  $sql2 = "SELECT Titre FROM `film` ORDER BY `film`.`Date_de_sortie` DESC LIMIT 10";
+  $requete2 = $db->query($sql2);
+  $filmNouv = $requete2->fetchAll();
+
+  $sql3 = "SELECT Titre FROM `série` ORDER BY `série`.`Note` DESC LIMIT 10";
+  $requete3 = $db->query($sql3);
+  $serieTop = $requete3->fetchAll();
+
+  $sql4 = "SELECT Titre FROM `série` ORDER BY `série`.`Date_de_sortie` DESC LIMIT 10";
+  $requete4 = $db->query($sql4);
+  $serieNouv = $requete4->fetchAll();
+
+  $sql5 = "SELECT Titre, Date_de_sortie, Durée, Description, Note FROM `film` ORDER BY `film`.`Note` DESC LIMIT 10";
+  $requete5 = $db->query($sql5);
+  $filmPlus = $requete5->fetchAll();
+
+  $sql6 = "SELECT Titre, Date_de_sortie, Description, Note FROM `série` ORDER BY `série`.`Note` DESC LIMIT 10";
+  $requete6 = $db->query($sql6);
+  $seriePlus = $requete6->fetchAll();
+
+function createTop10Film($filmTop){
+
+  foreach ($filmTop as $titre){
+    echo
+      '<div class="item text-center">' .
+        '<img src="src/images/picture_grey.svg" class="card-img-top" alt="...">' .
+        htmlspecialchars($titre['Titre']) . 
+       '</div>';
+  }
+}
+function createNouvFilm($filmNouv){
+
+  foreach ($filmNouv as $titre){
+    echo
+      '<div class="item text-center">' .
+        '<img src="src/images/picture_grey.svg" class="card-img-top" alt="...">' .
+        htmlspecialchars($titre['Titre']) . 
+       '</div>';
+  }
+}
+
+function createTop10Serie($serieTop){
+
+  foreach ($serieTop as $titre){
+    echo
+      '<div class="item text-center">' .
+        '<img src="src/images/picture_grey.svg" class="card-img-top" alt="...">' .
+        htmlspecialchars($titre['Titre']) . 
+       '</div>';
+  }
+}
+
+function createNouvSerie($serieNouv){
+
+  foreach ($serieNouv as $titre){
+    echo
+      '<div class="item text-center">' .
+        '<img src="src/images/picture_grey.svg" class="card-img-top" alt="...">' .
+        htmlspecialchars($titre['Titre']) . 
+       '</div>';
+  }
+}
+
+
+function createFilmPlus($filmPlus){
+
+  $active = ' active';
+  foreach ($filmPlus as $infoPlus){
+    echo
+    '<div class="carousel-item' . $active . '">' .
+        '<img src="src/images/picture_grey.svg" class="card-img-top" alt="...">' .
+      '<div class="item-body">' .
+        '<h1>' . htmlspecialchars($infoPlus['Titre']) . '</h1>' .
+        '<p>' . 'Sortie le: ' . htmlspecialchars($infoPlus['Date_de_sortie']) . '</p>' .
+        '<p>' . 'Durée: ' . htmlspecialchars($infoPlus['Durée']) . ' minute(s)'.'</p>' .
+        '<p>' . 'Description: ' . htmlspecialchars($infoPlus['Description']) . '</p>' .
+        '<p>' . 'Note: ' . htmlspecialchars($infoPlus['Note']) . '/10'.'</p>' .
+      '</div>' .
+    '</div>';
+  $active = '';
+  }
+}
+
+function createSeriePlus($seriePlus){
+
+  $active = ' active';
+  foreach ($seriePlus as $infoPlus){
+    echo
+    '<div class="carousel-item' . $active . '">' .
+        '<img src="src/images/picture_grey.svg" class="card-img-top" alt="...">' .
+      '<div class="item-body">' .
+        '<h1>' . htmlspecialchars($infoPlus['Titre']) . '</h1>' .
+        '<p>' . 'Sortie le: ' . htmlspecialchars($infoPlus['Date_de_sortie']) . '</p>' .
+        '<p>' . 'Description: ' . htmlspecialchars($infoPlus['Description']) . '</p>' .
+        '<p>' . 'Note: ' . htmlspecialchars($infoPlus['Note']) . '/10'.'</p>' .
+      '</div>' .
+    '</div>';
+  $active = '';
+  }
+}
+
+?>
