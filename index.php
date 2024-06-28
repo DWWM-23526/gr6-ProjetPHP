e697a0fa0c5171407d6300ffdddb6304440da2a8
Yassin-Rahmani <yassin.rahmani@gmail.com>
2024-06-28 17:09:18 +0200

    header, sidebar & footer ok

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
 <?php
-$page = $_SERVER['PHP_SELF'];
-$sec = "1";
+    include_once("./src/components/header.php");
+
+    include_once("./src/components/sidebar.php");
+
+    include_once("./src/components/footer.php");
 ?>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'"
-</head>
-<body>
-    <?php
-       
-    ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-</html>
---
+++ src/components/footer.php
@@ -0,0 +1,58 @@
+        </main>
+        <footer>
+            <div class="container">
+                <div class="footer-info">
+                    <div class="footer-socials">
+                        <div>
+                            <h3>
+                                Suivez-nous
+                            </h3>
+                        </div>
+                        <div class="socials">
+                            <div class="social">
+                                <img class="social-icon" src="./src/images/svg/facebook.svg" alt="social-logo">
+                            </div>
+                            <div class="social">
+                                <svg class="x-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+                            </div>
+                            <div class="social">
+                                <img class="instagram-icon" src="./src/images/svg/instagram.svg" alt="social-logo">
+                            </div>
+                            <div class="social">
+                                <img class="tiktok-icon" src="./src/images/svg/tiktok.svg" alt="social-logo">
+                            </div>
+                            <div class="social">
+                                <img class="social-icon" src="./src/images/svg/mail.svg" alt="social-logo">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-links">
+                        <p><a href="#">Contact</a></p>
+                        <p><a href="#">Qui sommes-nous?</a></p>
+                        <p><a href="#">CGU</a></p>
+                        <p><a href="#">Politique de cookies</a></p>
+                        <p><a href="#">Données personnelles</a></p>
+                    </div>
+                    <div class="footer-apps">
+                        <div>
+                            <h3>
+                                Critix Apps.
+                            </h3>
+                        </div>
+                        <div class="apps">
+                            <div class="app">
+                                <img class="app-icon" src="./src/images/svg/apple.svg" alt="app-icon">
+                            </div>
+                            <div class="app">
+                                <img class="android-icon" src="./src/images/svg/android.svg" alt="app-icon">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-logo">
+                    <img src="./src/images/critixLogo.webp" alt="logo">
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
